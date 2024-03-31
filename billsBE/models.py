39cd6c517e7721544f6b7bddf441b9469cdefd56
RachelY <yang.yijing@outlook.com>
2024-03-31 17:05:02 -0400

    updated the field for amount and due_date

--- billsBE/models.py
+++ billsBE/models.py
@@ -4,5 +4,5 @@ from django.db import models
 class Bills(models.Model):
     bill_name=models.CharField(max_length=100)
     due_date=models.DateField(null=True, blank=True)
-    amount=models.FloatField()
+    amount=models.DecimalField(max_digits=10, decimal_places=2)
     status=models.BooleanField(default=False)
